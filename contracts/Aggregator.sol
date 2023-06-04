c7b1ee49495af168ad6534a2cf3d70b5b7d0a99f
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-06-04 13:25:17 -0600

    depostis to aave

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -1,8 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Aggregator {
-    function getCurrentBlock() public view returns (uint256) {
-        return block.number;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import "@aave/core-v3/contracts/interfaces/IPool.sol";
+import "hardhat/console.sol";
+
+contract Aggregator is Ownable {
+    // might have to use constant
+    address public immutable AaveV3PoolAddressProvider =
+        0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e;
+    address public immutable WETH_Address =
+        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public immutable CompoundV3ProxyAddress =
+        0xA17581A9E3356d9A858b789D68B4d866e593aE94;
+    address public immutable AaaveWETH_Address =
+        0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8;
+
+    IERC20 immutable weth;
+    IERC20 immutable aaveAweth;
+
+    constructor() Ownable() {
+        weth = IERC20(WETH_Address);
+        aaveAweth = IERC20(AAVE_A_WETH_MAINNET_ADDRESS);
+        // comet = IComet(COMPOUND_V3_PROXY_MAINNET_ADDRESS);
+    }
+
+    function deposit(
+        uint256 _amount,
+        uint256 _compAPY,
+        uint256 _aaveAPY
+    ) external onlyOwner {
+        require(_amount > 0);
+
+        if (_compAPY > _aaveAPY) {
+            // Deposit into Compound
+            // require(_depositToCompound(_amount) == 0);
+            // Update location
+        } else {
+            // Deposit into Aave
+            _deposit_to_aave(_amount);
+        }
+    }
+
+    function _getAavePool() private view returns (IPool) {
+        address aaveV3PoolAddress = IPoolAddressesProvider(
+            AaveV3PoolAddressProvider
+        ).getPool();
+        return IPool(aaveV3PoolAddress);
+    }
+
+    function _deposit_to_aave(uint256 weth_amount) private {
+        IPool aavePool = _getAavePool();
+        weth.approve(address(aavePool), weth_amount);
+        console.log(address(aavePool));
+        aavePool.supply(address(weth), weth_amount, address(this), 0);
     }
 }
--- test/ aggregator.js
+++ test/ aggregator.js
@@ -1,36 +1,81 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { abi: IERC20_ABI } = require("@openzeppelin/contracts/build/contracts/IERC20.json");
 
 const cometABI = require('../ABIs/cometABI.json');
 const aaveV3PoolABI = require('../ABIs/aaveV3PoolABI.json');
 const getAPY = require('../helpers/getAPY.js');
 
+const WETH_Address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 const cWETHv3 = '0xA17581A9E3356d9A858b789D68B4d866e593aE94';
 const aaveV3Pool = '0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2'; // I think this is right
 
 describe("Aggregator", function () {
+  let WETH;
+  
 
   beforeEach(async () => {
     [owner, user] = await ethers.getSigners();
-  
     Aggregator = await ethers.getContractFactory("Aggregator");
     aggregator = await Aggregator.deploy();
     await aggregator.deployed();
 
+    WETH = new ethers.Contract(WETH_Address, IERC20_ABI, owner);
+    cometWETH_Contract = new ethers.Contract(WETH_Address, cometABI.abi, owner);
     cWETHv3_Contract = new ethers.Contract(cWETHv3, cometABI.abi, owner);
     aaveV3Pool_contract = new ethers.Contract(aaveV3Pool, aaveV3PoolABI, owner);
   });
 
-  it("fetches compound exchange rate", async function () {
-    const result = await getAPY.getCompoundAPY(cWETHv3_Contract);
-    console.log(result);
-    expect(result).to.not.equal(0);
+  it("gets WETH", async function () {
+    const amount = ethers.utils.parseEther('20');
+    await owner.sendTransaction({
+      to: WETH.address,
+      value: amount
+    });
+    const WETH_Balance = await WETH.balanceOf(owner.address)
+    expect(WETH_Balance.toString()).to.equal('20000000000000000000');
   });
 
-  it('fetches aave exchange rate', async () => {
-    let result = await getAPY.getAaveAPY(aaveV3Pool_contract)
-    console.log(result)
-    expect(result).to.not.equal(0);
-})
+  describe('APY rates', async () => {
+
+    it("fetches compound APY rate", async function () {
+      const result = await getAPY.getCompoundAPY(cWETHv3_Contract);
+      console.log(result);
+      expect(result).to.not.equal(0);
+    });
+
+    it('fetches aave APY rate', async () => {
+      let result = await getAPY.getAaveAPY(aaveV3Pool_contract)
+      console.log(result)
+      expect(result).to.not.equal(0);
+    })
+
+  });
+
+  describe("deposits", async function () {
+    let amount = 10;
+    // let amountInWei = utils.parseEther(amount.toString());
+    let compAPY, aaveAPY;
+    let result;
+
+      beforeEach(async function () {
+        compAPY = await getAPY.getCompoundAPY(cWETHv3_Contract);
+        aaveAPY = await getAPY.getAaveAPY(aaveV3Pool_contract)
+      });
+
+      it("deposit to aave", async function () {
+
+        await WETH.approve(aggregator.address, ethers.utils.parseEther('15'));
+        await WETH.transfer(aggregator.address, ethers.utils.parseEther('10'));
+        tx = await aggregator._deposit_to_aave(ethers.utils.parseEther('10'));
+        console.log(tx);
+        console.log(WETH)
+        
+
+      });
+
+
+        
+  });
 
 });
